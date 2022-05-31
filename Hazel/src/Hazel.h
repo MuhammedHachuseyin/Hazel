eaf690d58608a857c555bdb9ec9228bd672b96ef
mltde <mltde@HARD>
2022-05-31 22:49:09 +0300

    Basic Setu

---
+++ Hazel/src/Hazel.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//For USe by Hazel APPLICATIONS!
+
+#include "Hazel/Application.h"
+#include <stdio.h>
+
+//----Entry Point---------------
+#include "Hazel/EntryPoint.h";
+//------------------------------
+
---
+++ Hazel/src/Hazel/EntryPoint.h
@@ -0,0 +1,19 @@
+#pragma once
+
+
+#ifdef HZ_PLATFORM_WINDOWS
+
+extern Hazel::Application* Hazel::CreateApplication();
+
+
+void main(int argc,char** argv)
+{
+	
+	printf("Hazel");
+	auto app = Hazel::CreateApplication();
+	app->Run();
+	delete app;
+
+}
+
+#endif // HZ_PLATFORM_WINDOWS
